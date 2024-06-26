e058a2cef6f1dce9e885da0b2da3e6f74116ca12
Vikyath Shirva <vicky.fames@gmail.com>
2024-06-26 22:01:11 +0530

    dp lps using dp soution wed 26/06/24 10:01 pm

--- src/main/java/DynamicProgramming/longestPalindromicSubstring.java
+++ src/main/java/DynamicProgramming/longestPalindromicSubstring.java
@@ -38,10 +38,35 @@ public class longestPalindromicSubstring {
         return res;
     }
 
-
-
     //dynamic programming (On2)
-    
+    public String longestPalinDP(String s) {
+        int longest = 0;
+        int left = 0;
+        int right=1;
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        for(int i=s.length();i>=0;i--) {
+            for(int j=i;j<s.length();j++) {
+                if(i == j) {
+                    dp[i][j] = true;
+                } else if(s.charAt(i) == s.charAt(j)) {
+                    if(j-i == 1) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i+1][j-1];
+                    }
+                }
+
+                if(dp[i][j] && j-i+1 > longest) {
+                    longest = j-i+1;
+                    left = i;
+                    right = j+1;
+                }
+
+            }
+        }
+        return s.substring(left, right+1);
+    }
+
 
 
 

ef13722e511a6caf5254dc1d40f0237940366043
Vikyath Shirva <vicky.fames@gmail.com>
2024-06-29 16:09:30 +0530

    trees traversals cleanup sat 29/06/24 4:09 pm

--- src/main/java/Trees/diameter.java
+++ src/main/java/Trees/diameter.java
@@ -3,20 +3,33 @@ package Trees;
 public class diameter {
 
     int result = -1;
-    public int findDiameter(TreeNode<Integer> root) {
+
+    public int dia(TreeNode<Integer> root) {
+        if(root == null) {
+            return -1;
+        }
         dfs(root);
         return result;
     }
 
     public int dfs(TreeNode<Integer> root) {
-        if (root == null) {
-            return -1;
+        if(root == null) {
+            return 0;
         }
-        int left = dfs(root.left);
-        int right = dfs(root.right);
+        int left = height(root.left);
+        int right = height(root.right);
         result = Math.max(result, left + right);
         return Math.max(left, right);
-
     }
+
+
+    public int height(TreeNode<Integer> root) {
+        if(root == null) {
+            return 0;
+        }
+        return 1 + Math.max(height(root.left), height(root.right));
+    }
+
+
 
 }

ef5809baf16f486a66eeded97f6d263de98cafef
Vikyath Shirva <vicky.fames@gmail.com>
2024-06-29 18:50:00 +0530

    zigzag sat 29/06/24 6:49 pm

---
+++ src/main/java/Trees/Traversals/zigZag.java
@@ -0,0 +1,40 @@
+package Trees.Traversals;
+import Trees.TreeNode;
+import java.util.*;
+public class zigZag {
+
+
+    public List<List<Integer>> zigZagOrder(TreeNode<Integer> root) {
+        List<List<Integer>> res = new ArrayList<>();
+        Deque<TreeNode<Integer>> q = new ArrayDeque<>();
+        q.offer(root);
+
+        boolean ltor = true; 
+        while(!q.isEmpty()) {
+            int size = q.size(); 
+            List<Integer> tempList = new ArrayList<>();
+            int[] temp = new int[size];
+            for(int i=0;i<size;i++) {
+                TreeNode<Integer> node = q.poll();
+                int idx = ltor ? 0 : size-1-i;
+                temp[idx] = node.data;
+                if(node.left!= null) {
+                    q.offer(node.left);
+                }
+                if(node.right!= null) {
+                    q.offer(node.right);
+                }
+            }
+            ltor = !ltor;
+            for(int n : temp) {
+                tempList.add(n);
+            }
+            res.add(tempList);
+        }
+
+        return res;
+        
+
+    }
+    
+}
